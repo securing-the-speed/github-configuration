7c9558767dad60fd37a71abe3cb385805e84c5f9
Andrew Krug <andrewkrug@gmail.com>
2024-07-23 22:39:22 -0700

    import repo

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "github_organization_settings" "securing_the_speed" {
     members_can_create_public_pages = true
     members_can_create_private_pages = true
     members_can_fork_private_repositories = true
-    web_commit_signoff_required = true
+    web_commit_signoff_required = false
     advanced_security_enabled_for_new_repositories = false
     dependabot_alerts_enabled_for_new_repositories=  false
     dependabot_security_updates_enabled_for_new_repositories = false
